c4fe35c370520a3ccd160b610533f970a1e69a42
gold bird <pouria346h@gmail.com>
2016-05-30 22:25:13 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,13 +1,11 @@
 do
 
 function run(msg, matches)
-    return "Self-Bot v1 \n by BeatBot Team :) <3 \n @BeatBot_Team"
+    return "بابام پوریا"
 end 
 return {
   patterns = {
-    "^#version$",
-    "^#bot$",
-    "^#selfbot$"
+    "^سازنده$",
   },
   run = run
 }
